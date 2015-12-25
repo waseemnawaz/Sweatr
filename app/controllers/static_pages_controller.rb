700c07e1ecf28ca6d55c358268e07e08211e101c
Waseem Nawaz <waseemhijodenawaz@yahoo.es>
2015-12-25 23:53:59 +0100

    email form thank you

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -5,4 +5,13 @@ class StaticPagesController < ApplicationController
     @featured_product = Product.first
     @products = Product.limit(3)
   end
+  def thank_you
+  @name = params[:name]
+  @email = params[:email]
+  @message = params[:message]
+  ActionMailer::Base.mail(:from => @email,
+      :to => 'waseemhijodenawaz@yahoo.es',
+      :subject => "A new contact form message from #{@name}",
+      :body => @message).deliver_now
+  end
 end
